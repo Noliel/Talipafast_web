c77ebc110400d4f41d20932510745400ec76604a
Nolibert Eligan <nolibertjr.eligan@gmail.com>
2024-02-28 11:02:24 +0800

    fix the category but PAYPAL still have problem

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -3,6 +3,7 @@
 import useLayoutService from '@/lib/hooks/useLayout'
 import Link from 'next/link'
 import useSWR from 'swr'
+import Header from './header/Header'
 
 const Sidebar = () => {
   const { toggleDrawer } = useLayoutService()
@@ -12,18 +13,21 @@ const Sidebar = () => {
   if (!categories) return 'Loading...'
 
   return (
-    <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
-      <li>
-        <h2 className="text-xl">Shop By Department</h2>
-      </li>
-      {categories.map((category: string) => (
-        <li key={category}>
-          <Link href={`/search?category=${category}`} onClick={toggleDrawer}>
-            {category}
-          </Link>
+    <div className=" w-80 bg-base-200 ">
+      <Header />
+      <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+        <li>
+          <h2 className="text-xl">Shop By Department</h2>
         </li>
-      ))}
-    </ul>
+        {categories.map((category: string) => (
+          <li key={category}>
+            <Link href={`/search?category=${category}`} onClick={toggleDrawer}>
+              {category}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
